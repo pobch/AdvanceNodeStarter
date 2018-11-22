95fe7fce523ef789829cca9cb63310afae63a699
Pob <590650@gmail.com>
2018-11-22 07:07:25 +0700

    test logging in feature by mocking cookies

--- tests/header.test.js
+++ tests/header.test.js
@@ -19,3 +19,37 @@ test('display logo when navigate to the root url', async () => {
 
   expect(text).toEqual('Blogster')
 })
+
+test('display a log out button when an user logged in', async () => {
+  const mongoose = require('mongoose')
+  const Keygrip = require('keygrip')
+  const keys = require('../config/keys')
+
+  mongoose.Promise = global.Promise
+  mongoose.connect(keys.mongoURI, { useMongoClient: true })
+  const userSchema = new mongoose.Schema({
+    googleId: String,
+    displayName: String
+  })
+  mongoose.model('User', userSchema)
+  const User = mongoose.model('User')
+  const user = await new User({}).save()
+
+  const sessionObj = {
+    passport: {
+      user: user._id.toString()
+    }
+  }
+  const session = Buffer.from(JSON.stringify(sessionObj)).toString('base64')
+
+  const keygrip = new Keygrip([keys.cookieKey])
+  const sig = keygrip.sign('session=' + session)
+
+  await page.setCookie({ name: 'session', value: session })
+  await page.setCookie({ name: 'session.sig', value: sig })
+  await page.goto('http://localhost:3000/blogs')
+  await page.waitFor('a[href="/auth/logout"]')
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+
+  expect(text).toEqual('Logout')
+})
