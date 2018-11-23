60bc53b610ff7c22553a66c9cf8347ddc2b43157
Pob <590650@gmail.com>
2018-11-24 05:10:36 +0700

    add test when an user submits valid inputs

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -22,4 +22,27 @@ describe('When logged in', () => {
     const label = await page.$eval('form label', el => el.innerHTML)
     expect(label).toEqual('Blog Title')
   })
+
+  describe('And using valid inputs', () => {
+    beforeEach(async () => {
+      await page.type('input[name="title"]', 'New Title')
+      await page.type('input[name="content"]', 'New Content')
+      await page.click('form button[type="submit"]')
+    })
+
+    test('submitting takes user to review screen', async () => {
+      const text = await page.$eval('form h5', el => el.innerHTML)
+      expect(text).toEqual('Please confirm your entries')
+    })
+
+    test('submitting then saving adds a new blog to index page', async () => {
+      await page.click('button.green')
+      await page.waitFor('.card-content')
+      
+      const title = await page.$eval('.card-content .card-title', el => el.innerHTML)
+      const content = await page.$eval('.card-content p', el => el.innerHTML)
+      expect(title).toEqual('New Title')
+      expect(content).toEqual('New Content')
+    })
+  })
 })
